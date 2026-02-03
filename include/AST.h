fe8115cc1fc78da31d1a73180c0b4032253fc058
jonahmer22 <jonah@jonahmerriam.net>
2026-02-03 10:20:04 -0500

    created a basic format for AST

--- include/AST.h
+++ include/AST.h
@@ -4,17 +4,25 @@
 #include "tokenizer.h"
 
 typedef enum SyntaxType{
-
+	unary,
+	binary,
+	literal
 } SyntaxType;
 
 typedef struct Syntax{
+	SyntaxType type;
+	Token *tok;
 
+	struct Syntax *left;
+	struct Syntax *right;
 } Syntax;
 
 
 // file may have multiple ASTs for multiple functions
 typedef struct AST{
+	Syntax *headl;
 
+	size_t size;
 } AST;
 
 #endif
